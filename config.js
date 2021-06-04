a1ac2d98efd9daaf1d008fb49ee7dca79a80f84f
Minh Anh <85324974+MinhAnh-Hub@users.noreply.github.com>
2021-06-04 11:56:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Giang à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'Em có thể làm công chúa của anh được không 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Dạ, mình yêu nhau anh nhé',
+    btnNo: 'Không, mình nên làm bạn thôi :3',
+    question:'Dù có chuyện gì thì yêu anh, em nhé',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Anh biết mà 🥰. Yêu công chúa nhiều nhiều 😘😘',
+    messDesc: 'Tối nay mình call cho nhau nha.',
     btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.comh/MinAnhSg/' 
 }
